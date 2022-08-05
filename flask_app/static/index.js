0e7b139b2c404b33521c50f098861cd3fe7dc077
lrayan7 <102861125+lrayan7@users.noreply.github.com>
2022-08-05 14:32:39 +0300

    Add files via upload

--- flask_app/static/index.js
+++ flask_app/static/index.js
@@ -1,7 +1,5 @@
-var editor
 function run() { 
-    line = editor.getValue().split(' ')
-
+    line =  document.getElementById('inputreq').value.split(' ')
     action = line[0]
     table = line[1]
     values = [{}]
@@ -10,37 +8,26 @@ function run() {
     for(i = 0; i < len; i++){
         prop = array[i].split(':')[0]
         val = array[i].split(':')[1]
-        values[i] = { 
-            [prop+" "+String(val)]: "" 
-        };
+        values[i] = JSON.stringify({ 
+            [prop]: val 
+        });
     }
-    // values is an array of 
-    // [{prop: val},{pro: val}...]
-    console.log("first js ", values)
-   // Name_is_john_and_Age_is_19
-    var result = JSON.stringify( 
-        {
-            'Action' : action,
-            'Table'  : table,
-            '-'      : values
-            // 'More':  
+    var result = {
+            Action: action,
+            Table: table,
+            Value: values
         }
-    );
-    console.log("from js ", result);
-    $.ajax({
-        type: "POST", //rest Type
-        dataType: 'json', //mispelled
-        url: "http://localhost:5000/json",
-        async: false,
-        contentType: "application/json;", //  charset=utf-8
-        data: result
-    });
+    var xmlhttp = new XMLHttpRequest();   // new HttpRequest instance 
+    var theUrl = "http://localhost:5000/json";
+    xmlhttp.open("POST", theUrl);
+    xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xmlhttp.send(JSON.stringify(result));
 }
 
-window.onload= function (){
-    editor = CodeMirror.fromTextArea(document.getElementById('editor'), {
-        mode: "python",
-        theme: "darcula"
-    });
+// window.onload= function (){
+//     editor = CodeMirror.fromTextArea(document.getElementById('editor'), {
+//         mode: "python",
+//         theme: "darcula"
+//     });
 
-}
+// }
